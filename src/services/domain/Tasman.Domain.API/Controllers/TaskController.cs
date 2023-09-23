5d945f159cf36ff965f006de62217d79a3a33a0c
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2023-09-23 10:22:46 -0300

    fix(domain): removing bad request from parameterless get

--- src/services/domain/Tasman.Domain.API/Controllers/TaskController.cs
+++ src/services/domain/Tasman.Domain.API/Controllers/TaskController.cs
@@ -16,13 +16,11 @@ public class TasksController : ControllerBase
     /// Get all tasks
     /// </summary>
     /// <response code="200">Returns the resource data</response>
-    /// <response code="400">There is a problem with the request</response>
     /// <response code="500">An internal error occurred</response>
     [HttpGet("")]
     [Consumes("application/json")]
     [Produces("application/json")]
     [ProducesResponseType(typeof(ApiResponse), (int)HttpStatusCode.OK)]
-    [ProducesResponseType(typeof(ApiResponse), (int)HttpStatusCode.BadRequest)]
     [ProducesResponseType(typeof(ApiResponse), (int)HttpStatusCode.InternalServerError)]
     public async Task<IActionResult> GetAllTasks()
     {
